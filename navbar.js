c9cbe7243d296eb78ed6dd5eb29e59cb810c57e4
laiba ahsan <laiba.ahsan27@gmail.com>
2023-06-15 21:47:45 +0530

    navbar added

---
+++ navbar.js
@@ -0,0 +1,8 @@
+const but = document.querySelector('.button');
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".nav")
+const toggleNavBar=()=>{
+    nav_header.classList.toggle('activ');
+    but.classList.toggle('activu');
+}
+mobile_nav.addEventListener('click',()=>toggleNavBar())
